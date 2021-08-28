05a708ec550bbdd46977d51f527c7b3a8617e7d0
Clarence <me@clarence.tw>
2021-08-29 01:51:22 +0800

    Update ecs multi port

--- lib/cdk-ecs-web-stack.ts
+++ lib/cdk-ecs-web-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
-import * as rds from '@aws-cdk/aws-rds';
 
 export class CdkEcsWebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,16 +10,6 @@ export class CdkEcsWebStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, 'VPC', { natGateways: 1 });
     const cluster = new ecs.Cluster(this, "EcsCluster", { vpc });
 
-    const rdsInstance = new rds.DatabaseCluster(this, "Database", {
-      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_2_09_2 }),
-      instanceProps: {
-        instanceType: new ec2.InstanceType('t3.small'),
-        vpc,
-      },
-      defaultDatabaseName: 'web_database',
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
     const taskDefinition = new ecs.TaskDefinition(this, 'TaskDef', {
       memoryMiB: '512',
       cpu: '256',
@@ -28,27 +17,20 @@ export class CdkEcsWebStack extends cdk.Stack {
       compatibility: ecs.Compatibility.EC2_AND_FARGATE,
     });
 
-    taskDefinition.addContainer('web', {
+    taskDefinition.addContainer('nodejs-web-server', {
       image: ecs.ContainerImage.fromRegistry('clarencetw/nodejs-web-server'),
       memoryReservationMiB: 256,
-      portMappings: [{ containerPort: 80 }],
+      portMappings: [{ containerPort: 3000 }],
       logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'nodejs-web-server' }),
       environment: {
         NODE_ENV: "production",
-        PORT: "80",
-        DB_DATABASE: 'web_database'
       },
-      secrets: {
-        DB_HOST: ecs.Secret.fromSecretsManager(rdsInstance.secret!, "host"),
-        DB_USERNAME: ecs.Secret.fromSecretsManager(
-          rdsInstance.secret!,
-          "username"
-        ),
-        DB_PASSWORD: ecs.Secret.fromSecretsManager(
-          rdsInstance.secret!,
-          "password"
-        ),
-      },
+    });
+    taskDefinition.addContainer('docker-nginx-multi-port', {
+      image: ecs.ContainerImage.fromRegistry('clarencetw/docker-nginx-multi-port'),
+      memoryReservationMiB: 256,
+      portMappings: [{ containerPort: 80 }, { containerPort: 8000 }, { containerPort: 8001 }],
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'docker-nginx-multi-port' }),
     });
 
     const fargateService = new ecs.FargateService(this, 'FargateService', {
@@ -63,8 +45,6 @@ export class CdkEcsWebStack extends cdk.Stack {
       ],
     });
 
-    rdsInstance.connections.allowFrom(fargateService, rdsInstance.connections.defaultPort!, `allow ${fargateService.serviceName} to connect db`);
-
     const lb = new elbv2.ApplicationLoadBalancer(this, "LB", {
       vpc,
       internetFacing: true,
@@ -72,14 +52,38 @@ export class CdkEcsWebStack extends cdk.Stack {
     const listener = lb.addListener("Listener", { port: 80 });
     listener.addTargets('web', {
       port: 80,
-      targets: [fargateService],
+      targets: [fargateService.loadBalancerTarget({
+        containerName: 'docker-nginx-multi-port',
+        containerPort: 80
+      })]
+    });
+    const listener8000 = lb.addListener("listener8000", { port: 8000, protocol: elbv2.ApplicationProtocol.HTTP });
+    listener8000.addTargets('web_env', {
+      port: 8000,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [fargateService.loadBalancerTarget({
+        containerName: 'docker-nginx-multi-port',
+        containerPort: 8000
+      })]
+    });
+    const listener8001 = lb.addListener("listener8001", { port: 8001, protocol: elbv2.ApplicationProtocol.HTTP });
+    listener8001.addTargets('web_mysql', {
+      port: 8001,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [fargateService.loadBalancerTarget({
+        containerName: 'docker-nginx-multi-port',
+        containerPort: 8001
+      })]
     });
 
+    new cdk.CfnOutput(this, 'NGINX_Page', {
+      value: `http://${lb.loadBalancerDnsName}`
+    })
     new cdk.CfnOutput(this, 'MySQL_Page', {
-      value: `http://${lb.loadBalancerDnsName}/mysql`
+      value: `http://${lb.loadBalancerDnsName}:8000`
     })
     new cdk.CfnOutput(this, 'ENV_Page', {
-      value: `http://${lb.loadBalancerDnsName}/env`
+      value: `http://${lb.loadBalancerDnsName}:8001`
     })
   }
 }
